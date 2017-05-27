767c5a3740cc19d3196559cbd1c235d40389b171
jiangminb <jiangminb@chanjet.com>
2017-05-27 21:45:17 +0800

    zipkin测试

--- jim-framework-dubbo-consumer/src/main/java/com/jim/framework/dubbo/consumer/controller/ProductController.java
+++ jim-framework-dubbo-consumer/src/main/java/com/jim/framework/dubbo/consumer/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.jim.framework.dubbo.consumer.controller;
 
 import com.alibaba.dubbo.rpc.RpcContext;
+import com.google.common.base.Stopwatch;
 import com.jim.framework.dubbo.core.model.Product;
 import com.jim.framework.dubbo.core.service.ProductService;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.UnknownHostException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jiang on 2017/3/28.
@@ -28,17 +30,20 @@ public class ProductController {
     @RequestMapping("/{productId}")
     public Product getById(@PathVariable final long productId) throws UnknownHostException {
         logger.info("ProductController.getById");
+        Stopwatch stopwatch=Stopwatch.createStarted();
         Product product= this.productService.getByid(productId);
-        Product product2= this.productService.getByid(productId*2);
-        try {
-            Product product3= (Product) RpcContext.getContext().getFuture().get();
-            System.out.println();product3.getId();
-            return product3;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        stopwatch.stop();
+        logger.info("time:"+String.valueOf(stopwatch.elapsed(TimeUnit.MILLISECONDS)));
+//        Product product2= this.productService.getByid(productId*2);
+//        try {
+//            Product product3= (Product) RpcContext.getContext().getFuture().get();
+//            System.out.println();product3.getId();
+//            return product3;
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
 
         return product;
     }
