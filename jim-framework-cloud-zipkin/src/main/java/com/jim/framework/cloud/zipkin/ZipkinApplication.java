ad909b71e5df46c2c8460cfda0132eecef8ee91d
jiangmin <min.jiang@ishansong.com>
2018-09-28 00:41:46 +0800

    zipkin-server配置完善

--- jim-framework-cloud-zipkin/src/main/java/com/jim/framework/cloud/zipkin/ZipkinApplication.java
+++ jim-framework-cloud-zipkin/src/main/java/com/jim/framework/cloud/zipkin/ZipkinApplication.java
@@ -2,8 +2,10 @@ package com.jim.framework.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinApplication {
 
 	public static void main(String[] args) {
